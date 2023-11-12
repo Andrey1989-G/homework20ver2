319a7992e684daac1e63ee0d09b342ea9eab0f43
Andrey1989-G <goreglyad.andrey@gmail.com>
2023-11-12 13:51:40 +0500

    step two готово

--- hw192/for_hw192/views.py
+++ hw192/for_hw192/views.py
@@ -6,4 +6,9 @@ def home(request):
     return render(request, 'hw192/home.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name}, {phone}, {message}')
     return render(request, 'hw192/contacts.html')
