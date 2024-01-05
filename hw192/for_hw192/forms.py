7fc85cb96e0189b238d4911f8cc4c0628ab44cf0
Andrey1989-G <goreglyad.andrey@gmail.com>
2024-01-05 15:36:17 +0500

    домашка 22.1 проработал замечания

--- hw192/for_hw192/forms.py
+++ hw192/for_hw192/forms.py
@@ -3,25 +3,23 @@ from django import forms
 from for_hw192.models import Product, Version
 
 class ProductForm(forms.ModelForm):
+    ban_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
+                'бесплатно', 'обман', 'полиция', 'радар']
     class Meta:
         model = Product
         fields = '__all__'
 
     def clean_product_name(self):
-        ban_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
-                    'бесплатно', 'обман', 'полиция', 'радар']
         cleaned_data = self.cleaned_data['product_name']
-        for ban_word in ban_list:
+        for ban_word in self.ban_list:
             if ban_word in cleaned_data:
                 raise forms.ValidationError('В названии имеются запрещенные продукты')
 
         return cleaned_data
 
     def clean_description(self):
-        ban_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
-                    'бесплатно', 'обман', 'полиция', 'радар']
         cleaned_data = self.cleaned_data['description']
-        for ban_word in ban_list:
+        for ban_word in self.ban_list:
             if ban_word in cleaned_data:
                 raise forms.ValidationError('В описании имеются запрещенные продукты')
 
--- hw192/for_hw192/views.py
+++ hw192/for_hw192/views.py
@@ -106,39 +106,3 @@ class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('for_hw192:list_product')
 
-
-# class IndexListView(ListView):
-#     model = Product
-#     template_name = 'for_hw192/index.html'
-#
-# class ShopSingleDetailView(DetailView):
-#     model = Product
-#     template_name = 'for_hw192/shop-single.html'
-
-# def index(request):
-#     all_products = Product.objects.all()
-#     print(all_products)
-#     context = {
-#         'object_list': all_products
-#     }
-#     print(context)
-#     return render(request, 'hw192/index.html', context)
-
-# def shop_single(request, prod_id): #prod_id передается из адресной строки. должен быть одинаков с path('shop-single/<slug:prod_id>
-#     one_product = get_object_or_404(Product, pk=prod_id)
-#     context = {
-#         'object_list': one_product
-#     }
-#     print(context)
-#     return render(request, 'hw192/shop-single.html', context)
-
-# def base(request):
-#     return render(request, 'for_hw192/base.html')
-#
-# def contact(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'{name}, {phone}, {message}')
-#     return render(request, 'for_hw192/contact.html')
